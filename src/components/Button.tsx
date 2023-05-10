ce5454afd88451c68197206524b0573e1246cd30
Jack Pordi <jackel119@gmail.com>
2023-05-10 19:34:19 +0100

    wip

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,9 +16,9 @@ export const Button: FunctionComponent<Props> = ({
   <button
       disabled={disabled}
       type="button"
-      className={`${props.class!} text-sm md:text-md transition ease-in-out duration-200 text-white bg-indigo-600 hover:bg-indigo-800 focus:ring-4 focus:ring-blue font-medium rounded-lg py-3 px-3 disabled:bg-gray-300`}
+      className={`${props.class!} text-sm md:text-md transition ease-in-out duration-200 text-white bg-indigo-600 hover:bg-indigo-800 focus:ring-4 focus:ring-blue font-medium rounded-full py-3 px-3 disabled:bg-gray-300`}
       onClick={onClick}
     >
-      { text}
+      { text }
     </button>
 );
